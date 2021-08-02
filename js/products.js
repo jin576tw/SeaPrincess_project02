4d096692e7059c509e8bf8743b5b427215699f9a
jin576tw <jin576tw@gmail.com>
2021-08-02 14:12:40 +0800

    購物商城part01

---
+++ js/products.js
@@ -0,0 +1,90 @@
+$(document).ready(function () {
+
+
+
+    $('.Seafood_select').on({
+
+        click:function(){
+
+            if ($(window).width() <  992){
+
+                $(this).css('background-color','white').css('opacity','1')
+                $(this).children('.Prouduct_select_icon').css('transform', 'scale(1.5) translateY(-5px)')
+
+                $(this).next().css('background-color','#DDDDDD').css('opacity','0.5')
+                $(this).next().children('.Prouduct_select_icon').css('transform', 'scale(1) translateY(0px)')
+
+
+
+            }
+
+            if ($(window).width() >= 992){
+
+                $(this).css('background-color','white').css('opacity','1')
+                $(this).children('.Prouduct_select_icon').css('transform', 'scale(1.8) translateY(-10px)')
+
+                $(this).next().css('background-color','#DDDDDD').css('opacity','0.5')
+                $(this).next().children('.Prouduct_select_icon').css('transform', 'scale(1) translateY(0px)')
+
+
+
+            }
+
+            
+
+
+        }
+
+
+
+    })
+
+    $('.Fishing_select').on({
+
+        click:function(){
+
+            if ($(window).width() <  992){
+
+                $(this).css('background-color','white').css('opacity','1')
+                $(this).children('.Prouduct_select_icon').css('transform', 'scale(1.5) translateY(-5px)')
+
+                $(this).prev().css('background-color','#DDDDDD').css('opacity','0.5')
+                $(this).prev().children('.Prouduct_select_icon').css('transform', 'scale(1) translateY(0px)')
+
+
+                
+            }
+
+
+            if ($(window).width() >= 992){
+
+            $(this).css('background-color','white').css('opacity','1')
+            $(this).children('.Prouduct_select_icon').css('transform', 'scale(1.8) translateY(-10px)')
+
+            $(this).prev().css('background-color','#DDDDDD').css('opacity','0.5')
+            $(this).prev().children('.Prouduct_select_icon').css('transform', 'scale(1) translateY(0px)')
+
+
+            
+            }
+
+
+
+        }
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+})
