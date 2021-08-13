b53d668fc8665ceae26d6c91a0c69926e0011b58
jin576tw <jin576tw@gmail.com>
2021-08-13 20:57:12 +0800

    購物車架構完成

--- js/All_checkout.js
+++ js/All_checkout.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
 
-    let check =`<i class="fas fa-check"></i>`;
+  
+    let bounce = `animate__bounceIn animate__animated`;
 
     // 第一步驟成功
     $('.cart_checkout_btn').on({
 
 
         click: function(){
+           
+            let check =`<i class="fas fa-check check01"></i>`;
 
             $('body,html').animate({
                 scrollTop: 0
@@ -22,12 +25,61 @@ $(document).ready(function () {
             $('.step_number02').css('background-color','var(--dark_blue)').css('transition','2.5s')
 
             $('.step_name02 p').css('color','var(--dark_blue)').css('transition','2.5s')
+
+            setTimeout(() => {
+                $('.check01').addClass(bounce)
+
+            }, 700);
         
            
             
         }
 
     })
+
+        // 第二步驟成功
+        $('.final_checkout_btn').on({
+
+           
+
+            click: function(){
+                let check =`<i class="fas fa-check check02"></i>`;
+
+                $('.fa-check').removeClass(bounce)
+
+            
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 1 ,'swing');
+    
+                $('.Checkout_step02').fadeOut(1000)
+                $('.FinishCheck_step03').fadeIn(500)
+    
+                $('.Checkout_Step_line').css('width','100%')
+    
+                $('.step_number02').html(check)
+    
+                $('.step_number03').css('background-color','var(--dark_blue)').css('transition','2.5s')
+    
+                $('.step_name03 p').css('color','var(--dark_blue)').css('transition','2.5s')
+
+                setTimeout(() => {
+                    $('.check02').addClass(bounce)
+                   
+
+                }, 800);
+
+                setTimeout(() => {
+                   
+                    $('.check03').addClass(bounce)
+
+                }, 1000);
+            
+               
+                
+            }
+    
+        })
 
 
 
