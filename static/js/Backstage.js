1c7e5d1cc2bbb03d7345bc9380923f04200e7ffb
jin576tw <jin576tw@gmail.com>
2021-10-22 17:17:03 +0800

    後台切版中

--- static/js/Backstage.js
+++ static/js/Backstage.js
@@ -1,1 +1,18 @@
 'use strict'
+
+
+
+if($(window).width() < 768){
+
+
+    $('#RWD_btn').click(function(){
+
+
+        $('.Backstage_bar').toggleClass('RWD_Backstage_barOpnen')
+       
+       
+       
+       
+       })
+}
+
